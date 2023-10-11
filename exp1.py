5e805abaa1c9fec9b76f333177061bcf269f1747
kanavsharma22 <147626743+kanavsharma22@users.noreply.github.com>
2023-10-11 19:17:15 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,29 @@
+#abs
+a=-69
+print("Absolute value of ",a," = ",abs(a))
+
+#len
+b="Farheen"
+print("Length of ",b," = ",len(b))
+
+#min
+c1=88
+c2=59
+print ("Minimum number between", c1, "and", c2 ,"= :", min (c1,c2) )
+
+#round
+d=89.52
+print("Round off of ",d," = ",round(d))
+
+#isalnum
+e="xyz135"
+e1=e.isalnum()
+if e1==True:
+    print(e,"is alphanumeric")
+else:
+    print(e,"is not alphanumeric")
+
+#type
+f=96
+print("type of ",f," = ",type(f))
+
